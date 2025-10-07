6d280c4e42ce31d0be67596d037569836b93920c
AjStyles1202 <158758601+AjStyles1202@users.noreply.github.com>
2025-10-07 15:38:54 +0530

    Update langchain_helper.py

--- langchain_helper.py
+++ langchain_helper.py
@@ -19,7 +19,7 @@ load_dotenv()  # take environment variables from .env (especially openai api key
 
 def get_few_shot_db_chain():
     db_user = "root"
-    db_password = "Timmy123!"
+    db_password = "Ayush123!"
     db_host = "localhost"
     db_name = "atliq_tshirts"
 
